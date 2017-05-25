b536b4fb385bfebd6eb25aedaaeb5754495f357e
jiulinghouxiaoyan <1406487226@qq.com>
2017-05-25 14:45:33 +0800

    新论文相似性检测本地上传

--- testRun/run_newPaperCheck_manualEntry.py
+++ testRun/run_newPaperCheck_manualEntry.py
@@ -26,8 +26,9 @@ class RunNewPaperCheck(myUnitChrome.UnitChrome):
         me.in_PaperCheck_button()
         sleep(2)
         me.in_manualEnter()
+        sleep(5)
 
-    def test_click_movetofolder_run1(self):
+    def test_click_addtofolder_run1(self):
         '''点击添加论文至分类文件夹，弹出添加文件夹窗口'''
         me=ManualEnterPaperCheck(self.driver)
         self.user_login()
@@ -39,7 +40,7 @@ class RunNewPaperCheck(myUnitChrome.UnitChrome):
         imagetest = getResultImage()
         imagetest.insert_image(self.driver,"click_addtofolder.jpg")
 
-    def test_moveto_existfolder_run(self):
+    def test_addto_existfolder_run2(self):
         '''添加至已有文件夹，显示文件夹名称'''
         me=ManualEnterPaperCheck(self.driver)
         self.user_login()
@@ -58,9 +59,9 @@ class RunNewPaperCheck(myUnitChrome.UnitChrome):
         print("添加至已存在文件夹测试成功")
         sleep(2)
         imagetest = getResultImage()
-        imagetest.insert_image(self.driver,"moveto_existfolder.jpg")
+        imagetest.insert_image(self.driver,"addto_existfolder.jpg")
 
-    def test_modifyto_existfolder_run(self):
+    def test_modify_existfolder_run3(self):
         '''修改至已有文件夹，显示文件夹名称'''
         me=ManualEnterPaperCheck(self.driver)
         self.user_login()
@@ -76,7 +77,7 @@ class RunNewPaperCheck(myUnitChrome.UnitChrome):
         me.modify_choosefolder_button()
         sleep(2)
         me.modify_existfolder()
-        sleep(2)
+        sleep(5)
         #获取选择文件夹的名称
         modify_name=me.modify_existfolder_text()
         sleep(2)
@@ -88,13 +89,12 @@ class RunNewPaperCheck(myUnitChrome.UnitChrome):
         print("修改文件夹测试成功")
         sleep(2)
         imagetest = getResultImage()
-        imagetest.insert_image(self.driver,"modifyto_existfolder.jpg")
+        imagetest.insert_image(self.driver,"modify_existfolder.jpg")
 
-    def test_add_foldername_success_run(self):
+    def test_addtonew_foldername_success_run4(self):
         '''添加至新文件夹成功'''
         me=ManualEnterPaperCheck(self.driver)
         self.user_login()
-        me.add_to_folder()
         sleep(5)
         foldername=me.add_to_new_folder()
         sleep(2)
@@ -106,9 +106,9 @@ class RunNewPaperCheck(myUnitChrome.UnitChrome):
 
         sleep(2)
         imagetest = getResultImage()
-        imagetest.insert_image(self.driver,"add_foldername_success.jpg")
+        imagetest.insert_image(self.driver,"addtonew_foldername_success.jpg")
 
-    def test_add_foldername_overlength_run(self):
+    def test_addtonew_foldername_overlength_run5(self):
         '''添加至新文件夹时，名称超长'''
         me=ManualEnterPaperCheck(self.driver)
         self.user_login()
@@ -120,9 +120,9 @@ class RunNewPaperCheck(myUnitChrome.UnitChrome):
         print("新建文件夹名称不能过长测试成功！")
         sleep(2)
         imagetest = getResultImage()
-        imagetest.insert_image(self.driver,"add_foldername_overlength.jpg")
+        imagetest.insert_image(self.driver,"addtonew_foldername_overlength.jpg")
 
-    def test_add_foldername_isnull_run(self):
+    def test_addtonew_foldername_isnull_run6(self):
         '''添加至新文件夹时，名称为空'''
         me=ManualEnterPaperCheck(self.driver)
         self.user_login()
@@ -133,9 +133,9 @@ class RunNewPaperCheck(myUnitChrome.UnitChrome):
         print("新建文件夹名称不能为空测试成功")
         sleep(2)
         imagetest = getResultImage()
-        imagetest.insert_image(self.driver,"add_foldername_isnull.jpg")
+        imagetest.insert_image(self.driver,"addtonew_foldername_isnull.jpg")
 
-    def test_add_foldername_exist_run(self):
+    def test_addtonew_foldername_exist_run7(self):
         '''添加至新文件夹时，名称已存在'''
         me=ManualEnterPaperCheck(self.driver)
         self.user_login()
@@ -144,6 +144,7 @@ class RunNewPaperCheck(myUnitChrome.UnitChrome):
         foldername=me.add_to_new_folder()
 
         me.modify_choosefolder_button()
+        sleep(2)
         #单击添加到新文件夹
         me.add_to_newfolder()
         sleep(2)
@@ -158,6 +159,140 @@ class RunNewPaperCheck(myUnitChrome.UnitChrome):
         print("新建文件夹名称不能重复测试成功")
         sleep(2)
         imagetest = getResultImage()
-        imagetest.insert_image(self.driver,"add_foldername_duplication.jpg")
+        imagetest.insert_image(self.driver,"addtonew_foldername_exist.jpg")
+
+    def test_addPaper_title_isNull_run8(self):
+        '''添加论文-题名为空'''
+        me=ManualEnterPaperCheck(self.driver)
+        self.user_login()
+        sleep(5)
+        me.input_paperInfo(title="",author="李硕",content="添加至已存在文件夹测试成功")
+        sleep(2)
+        me.click_startCheck()
+        sleep(2)
+        self.assertEqual(me.title_error_remind(),"这是必填字段")
+        print("题名为空测试成功")
+
+    def test_addPaper_author_isNull_run9(self):
+        '''添加论文-作者为空'''
+        me=ManualEnterPaperCheck(self.driver)
+        self.user_login()
+        sleep(5)
+        me.input_paperInfo(title="野火烧不尽",author="",content="添加至已存在文件夹测试成功")
+        sleep(2)
+        me.click_startCheck()
+        sleep(2)
+        self.assertEqual(me.author_error_remind(),"这是必填字段")
+        print("作者为空测试成功")
+
+    def test_addPaper_content_isNull_run10(self):
+        '''添加论文-内容为空'''
+        me=ManualEnterPaperCheck(self.driver)
+        self.user_login()
+        sleep(5)
+        me.input_paperInfo(title="野火烧不尽",author="李硕",content="")
+        sleep(2)
+        me.click_startCheck()
+        sleep(2)
+        self.assertEqual(me.content_error_remind(),"这是必填字段")
+        print("文本内容为空测试成功")
+
+    def test_addPaper_title_more50_run11(self):
+        '''添加论文-题名超过50位字符'''
+        me=ManualEnterPaperCheck(self.driver)
+        self.user_login()
+        sleep(5)
+        me.paper_title("策划项目规划管理服务，并输出策划决策分析报告。策划决策分析服务实现基于模板决策分析和自定义决策分析两种")
+        #me.input_paperInfo(title="策划项目规划管理服务，并输出策划决策分析报告。策划决策分析服务实现基于模板决策分析和自定义决策分析两种",author="李硕",content="添加至已存在文件夹测试成功")
+        me.paper_author("")
+        sleep(2)
+        me.click_startCheck()
+        sleep(2)
+        self.assertEqual(me.get_title_remind(),"题名长度不能大于50个字符")
+        print("题名不能超过50字符测试成功")
+
+    def test_addPaper_author_more30_run12(self):
+        '''添加论文-作者名超过30位字符'''
+        me=ManualEnterPaperCheck(self.driver)
+        self.user_login()
+        sleep(5)
+        me.paper_title("策划项目规划管理服务")
+        me.paper_author("策划项目规划管理服务，并输出策划决策分析报告。策划决策分析服务")
+        me.paper_content("")
+        sleep(2)
+        me.click_startCheck()
+        sleep(2)
+        self.assertEqual(me.get_author_remind(),"作者名称不能大于30个字符")
+        print("作者不能超过30字符测试成功")
+
+
+    def test_addPaper_content_more20W_run14(self):
+        '''文本内容超过20W'''
+        me=ManualEnterPaperCheck(self.driver)
+        self.user_login()
+        sleep(5)
+        me.paper_title("野火烧不尽")
+        me.paper_author("李硕")
+        sleep(2)
+        me.readAndInputContent("text.txt")
+        sleep(5)
+        me.click_startCheck()
+        sleep(15)
+        self.assertEqual(me.verifyExistAlert(),"文本长度大于20万字")
+        print("文本不能超过20W字符测试成功")
+
+    def test_clearPaper_run13(self):
+        '''清空文本'''
+        me=ManualEnterPaperCheck(self.driver)
+        self.user_login()
+        sleep(5)
+        me.paper_title("野火烧不尽")
+        me.paper_author("李硕")
+        me.paper_content("策划项目规划管理服务，并输出策划决策分析报告。策划决策分析服务")
+        sleep(2)
+        me.click_clearPaper()
+        sleep(2)
+        me.click_startCheck()
+        sleep(2)
+        self.assertEqual(me.content_error_remind(),"这是必填字段","没有清空文本")
+        print("清空文本测试成功")
+
+    def test_startCheck_lookresult(self):
+        '''开始检测-跳转到检测信息页'''
+        me=ManualEnterPaperCheck(self.driver)
+        self.user_login()
+        sleep(5)
+        me.input_paperInfo(title="泥石流",author="李硕",content="添加至已存在文件夹测试成功")
+        sleep(2)
+        me.click_startCheck()
+        sleep(2)
+        me.jump_checkInfo()
+        sleep(5)
+        self.assertEqual(me.checkInfo_title(),"泥石流")
+
+    def test_startCheck_continue(self):
+        '''开始检测-继续检测论文'''
+        me=ManualEnterPaperCheck(self.driver)
+        self.user_login()
+        sleep(5)
+        me.input_paperInfo(title="泥石流",author="李硕",content="添加至已存在文件夹测试成功")
+        sleep(2)
+        me.click_startCheck()
+        sleep(2)
+        me.continue_check()
+        sleep(2)
+        self.assertEqual(me.paperCheck_window(),"新论文相似性检测")
+
+
+
+
+
+
+
+
+
+
+
+
 
 
